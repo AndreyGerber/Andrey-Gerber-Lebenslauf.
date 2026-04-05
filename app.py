2d1ba0716284a37ee35132fc66e4595938032511
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-04-05 14:51:47 +0000

    update

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ with st.container(height=BLOCK_HOEHE, border=True):
 
     elif jahr_aktiv == 1996:
         # --- DEINE PARAMETER ---
-        MASSSTAB = 0.7  # Passe diesen Wert an, falls das Bild zu groß ist
+        MASSSTAB = 1.2  # Passe diesen Wert an, falls das Bild zu groß ist
 
         # Wir erstellen zwei Spalten
         col_text, col_foto = st.columns([1, 2])
