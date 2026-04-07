51a8216bc493f95f9514ecdcefcca4420a66ff25
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-04-07 09:11:19 +0000

    update

--- app.py
+++ app.py
@@ -611,7 +611,7 @@ st.markdown("""
     
     /* Icons vergrößern und fixieren */
     div.stButton > button p {
-        font-size: 20px !important;
+        font-size: 18px !important;
         margin: 0 !important;
         line-height: 1.2 !important;
         display: block;
