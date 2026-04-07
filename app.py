d83672b8f60f49c727aee3c3929b4ff1de9be498
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-04-07 14:51:22 +0000

    update

--- app.py
+++ app.py
@@ -645,9 +645,10 @@ with col_gallery:
     for i, d in enumerate(other_docs):
         with grid_cols[i % 3]: 
             render_btn(d)
-        st.vertical_spacer() # Falls vorhanden, sonst:
-    st.write("") 
-    st.container() 
+    
+     st.write("-") 
+     st.write("-") 
+     st.write("-") 
 
 with col_viewer:
     pdf_b64 = get_pdf_base64(st.session_state.active_doc)
