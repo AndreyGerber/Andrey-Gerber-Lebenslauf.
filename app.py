30bc441bbe05b7ff7f0c60e10242d667aaccf3e6
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-04-08 16:57:59 +0000

    update

--- app.py
+++ app.py
@@ -319,7 +319,7 @@ with st.container(height=BLOCK_HOEHE, border=True):
         with col_text:
             st.markdown(f"<div style='margin-top: {OBEN_ABSTAND};'></div>", unsafe_allow_html=True)
             st.subheader(f"🎒 {jahr_aktiv}: Schulzeit")
-            st.markdown(f"<p style='font-size: {INFO_FONT_SIZE}; color: #4B0082;'>So schnell vergehen 10 Jahre.</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size: {INFO_FONT_SIZE}; color: #0055A5;'>So schnell vergehen 10 Jahre.</p>", unsafe_allow_html=True)
 
         with col_foto:
             img_schule = lade_formatiertes_bild("schule2.png")
