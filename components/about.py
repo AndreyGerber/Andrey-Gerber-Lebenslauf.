2a4a2d39d8568f71018ed955b58366110ea83fe8
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-05-19 10:40:14 +0000

    update

--- components/about.py
+++ components/about.py
@@ -3,4 +3,16 @@ import streamlit as st
 def show_about(t):
     st.markdown("---")
     st.markdown(f"## {t['about_title']}")
-    st.write(t["about_text"])
+
+    st.markdown(
+        f"""
+        <p style="
+            font-size:18px;
+            line-height:1.6;
+            text-align:justify;
+        ">
+            {t['about_text']}
+        </p>
+        """,
+        unsafe_allow_html=True
+    )
