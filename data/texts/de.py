2739f0b17b92e7024b4e38073e8b62180ce34b28
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-05-19 10:11:43 +0000

    update

---
+++ data/texts/de.py
@@ -0,0 +1,4 @@
+TEXTS = {
+    "welcome": "Willkommen auf der Seite",
+    "title": "Lebenslauf von Andrey Gerber",
+}
---
+++ data/texts/en.py
@@ -0,0 +1,5 @@
+TEXTS = {
+    "welcome": "Welcome to the page",
+    "title": "CV of Andrey Gerber",
+}
+
